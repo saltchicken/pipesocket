107a8298afeb457ff48831fca29986d31131274a
saltchicken <john.eicher89@gmail.com>
2024-04-14 17:10:35 -0700

    Fix setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # with open("README.md", "r", encoding="utf-8") as fh:
 #     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name='pipesocket',
     version='0.0.1',
     author='John Eicher',
