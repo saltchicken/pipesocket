ebeca1e54b74a07c8ac2f21758dce6f69c1ebed0
saltchicken <john.eicher89@gmail.com>
2024-04-14 17:19:32 -0700

    Bring in codebase

---
+++ pipesocket/classes.py
@@ -0,0 +1,140 @@
+import asyncio
+import websockets
+from websockets.exceptions import ConnectionClosed
+import multiprocessing, queue, asyncio
+from loguru import logger
+
+
+class Client():
+    def __init__(self):
+        pass
+
+    async def send_routine(self, ws):
+        # TODO: How can I properly break this loop?
+        while True:
+            try:
+                output = await self.send()
+                if not output: continue
+                if output == 'quit':
+                    logger.debug('Received quit')
+                    break
+                # logger.debug(f"You entered: {user_input}")
+                await ws.send(output)
+            except websockets.exceptions.ConnectionClosedOK:
+                logger.warning('Connection was already closed. Breaking')
+                break
+
+    # async def get_user_input(self):
+    #     loop = asyncio.get_event_loop()
+    #     user_input = await loop.run_in_executor(None, input, "Enter something: ")
+    #     return user_input
+
+    async def receive_routine(self, ws):
+        try:
+            while True:
+                message = await ws.recv()
+                # TODO: self.process_input has to return True or else connection is broken.
+                result = await self.process_input(message)
+                if not result: break
+        except ConnectionClosed:
+            # TODO: This needs to quit the client
+            logger.debug("Server closed connection")
+        except asyncio.CancelledError:
+            logger.debug("Routine cancelled")
+
+    async def process_input(self, input):
+        if input == 'quit':
+            logger.debug('Quit received')
+            return False
+        else:
+            logger.debug(f'Processing: {input}')
+            return True
+        
+    async def send(self):
+        # await asyncio.sleep(3)
+        # return "test"
+    
+        loop = asyncio.get_event_loop()
+        output = await loop.run_in_executor(None, input, 'hello:')
+        return output
+
+
+
+    async def main(self):
+        uri = "ws://localhost:8765"
+        async with websockets.connect(uri) as websocket:
+            # send_task = asyncio.create_task(send_routine(websocket))
+            task = asyncio.create_task(self.receive_routine(websocket))
+            send_task = self.send_routine(websocket)
+            # task = receive_routine(websocket)
+            await send_task
+            task.cancel()
+            logger.debug("End of main reached")
+            # await asyncio.Future()
+
+    def run(self):
+        asyncio.run(self.main())
+
+
+class Server():
+    def __init__(self):
+        self.connected_clients = set()
+
+    async def handle_client(self, websocket, path):
+        logger.debug(f"{path} connected")
+        self.connected_clients.add(websocket)
+        try:
+            while True:
+                message = await websocket.recv()
+                # TODO: Run this in its own thread? Why does it block?
+                result = await self.process_input(message)
+                # loop = asyncio.get_event_loop()
+                # result = await loop.run_in_executor(None, self.process_input, message)
+        except ConnectionClosed:
+            logger.debug(f"{path} disconnected")
+            self.connected_clients.remove(websocket)
+
+    async def send_routine(self):
+        # TODO: How can I properly break this loop?
+        while True:
+            # TODO: Use next 2 lines for synchronous action.
+            # loop = asyncio.get_event_loop()
+            # output = await loop.run_in_executor(None, self.send)
+            output = await self.send()
+            if not output: continue
+            if output == 'quit':
+                for ws in self.connected_clients:
+                    await ws.send('quit')
+                break
+            for ws in self.connected_clients:
+                await ws.send(output)
+    
+    # def send(self):
+    #     return input('Enter something yea:? ')
+    
+    # def process_input(self, input):
+    #     time.sleep(3)
+    #     logger.debug(input)
+
+    async def send(self):
+        await asyncio.sleep(3)
+        return "test"
+    
+        # loop = asyncio.get_event_loop()
+        # output = await loop.run_in_executor(None, input, 'hello:')
+        # return output
+
+    async def process_input(self, input):
+        await asyncio.sleep(3)
+        logger.debug(input)
+
+    async def main(self):
+        async with websockets.serve(self.handle_client, "localhost", 8765):
+            # asyncio.create_task(send_routine())
+            # await asyncio.Future()
+            task = self.send_routine()
+            result = await task
+            logger.debug("End of main reached")
+
+    def run(self):
+        asyncio.run(self.main())
---
+++ pipesocket/experimental.py
@@ -0,0 +1,75 @@
+import asyncio
+import multiprocessing, queue, asyncio
+from loguru import logger
+
+from classes import Server, Client
+
+class Controller():
+    def __init__(self, server_bool = False):
+        self._send_q = multiprocessing.Queue()
+        self._receive_q = multiprocessing.Queue()
+        if server_bool:
+            self._process = multiprocessing.Process(target=self.start_server)
+        else:
+            self._process = multiprocessing.Process(target=self.start_client)
+        self._process.start()
+    
+    def put(self, message):
+        self._send_q.put(message)
+
+    def get(self):
+        message = self._receive_q.get()
+        return message
+    
+    def start_server(self):
+        self.server = BetterServer(self._send_q, self._receive_q)
+        self.server.run()
+
+    def start_client(self):
+        self.client = BetterClient(self._send_q, self._receive_q)
+        self.client.run()
+
+
+class BetterServer(Server):
+    def __init__(self, send_q, receive_q):
+        super().__init__()
+        self.send_q = send_q
+        self.receive_q = receive_q
+    
+    async def process_input(self, input):
+        # return super().process_input(input)
+        logger.debug(f"Client received: {input}")
+        self.receive_q.put(input)
+        return True
+
+    async def send(self):
+        try:
+            message = self.send_q.get_nowait()
+            logger.debug(message)
+            return message
+        except queue.Empty:
+            # logger.debug("Queue was empty")
+            await asyncio.sleep(0.3)
+            # return "Nope"
+
+class BetterClient(Client):
+    def __init__(self, send_q, receive_q):
+        super().__init__()
+        self.send_q = send_q
+        self.receive_q = receive_q
+
+    async def process_input(self, input):
+        # return super().process_input(input)
+        logger.debug(f"Client received: {input}")
+        self.receive_q.put(input)
+        return True
+
+    async def send(self):
+        try:
+            message = self.send_q.get_nowait()
+            logger.debug(message)
+            return message
+        except queue.Empty:
+            # logger.debug("Queue was empty")
+            await asyncio.sleep(0.3)
+            # return "Nope"
